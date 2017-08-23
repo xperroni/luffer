75757d3441d86fc31d5658cd110cea27041ed28a
Helio Perroni Filho <mail@xperroni.me>
2017-08-23 20:36:33 -0300

    bugfix: Order of setup commands
    Originally the `host.bashrc` configuration file was read before the
    current directory was set; this could lead to setup issues. Fixed.
    
    Signed-off-by: Helio Perroni Filho <mail@xperroni.me>

--- scripts/dokka-exec.sh
+++ scripts/dokka-exec.sh
@@ -4,7 +4,7 @@ export EXEC_BASHRC="$DOKKA_PLUGGED_DIR/exec.bashrc"
 
 if [ -e "$EXEC_BASHRC" ]
 then
-    docker exec -ti $DOKKA_PLUGGED bash -c "source $EXEC_BASHRC ; cd $(pwd) ; $*"
+    docker exec -ti $DOKKA_PLUGGED bash -c "cd $(pwd) ; source $EXEC_BASHRC ; $*"
 else
     docker exec -ti $DOKKA_PLUGGED bash -c "cd $(pwd) ; $*"
 fi
