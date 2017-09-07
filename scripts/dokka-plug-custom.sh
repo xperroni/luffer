b4e87a9746f06667c89d395bfffedb7fa25fa1c9
Helio Perroni Filho <mail@xperroni.me>
2017-09-07 15:11:36 -0300

    feature: Map home folder path in the container to host folder
    Some tools need to create subfolders inside the user's home folder to
    store settings and other files. Originally this wasn't possible.
    Enabled by adding a volume mapping option to the docker call.
    
    Signed-off-by: Helio Perroni Filho <mail@xperroni.me>

--- scripts/dokka-plug-custom.sh
+++ scripts/dokka-plug-custom.sh
@@ -3,6 +3,7 @@
 $DOKKA_HOME/dokka-run.sh $* --name $DOKKA_PLUGGED -d \
     --user="$(id -u):$(id -g)" \
     $(for gid in $(id -G); do echo -n "--group-add $gid "; done) \
+    --volume="$DOKKA_PLUGGED_HOME:$HOME" \
     --volume="/etc/group:/etc/group:ro" \
     --volume="/etc/passwd:/etc/passwd:ro" \
     --volume="/etc/shadow:/etc/shadow:ro" \
--- scripts/dokka-plug.sh
+++ scripts/dokka-plug.sh
@@ -2,13 +2,16 @@
 
 export DOKKA_IMAGE=$1
 export DOKKA_PLUGGED=${1//[:\/]/_}
-export DOKKA_PLUGGED_DIR="$DOKKA_HOME/${DOKKA_IMAGE//://}"
+export DOKKA_PLUGGED_DIR="$DOKKA_HOME/${1//://}"
+export DOKKA_PLUGGED_HOME="$DOKKA_PLUGGED_DIR/home"
+
+mkdir -p "$DOKKA_PLUGGED_HOME"
 
 PLUG_CUSTOM="$DOKKA_HOME/${1//://}/plug.sh"
 
 if [ -e "$PLUG_CUSTOM" ]
 then
-    $PLUG_CUSTOM
+    $PLUG_CUSTOM $1 bash "${@:2}"
 else
     $DOKKA_HOME/dokka-plug-custom.sh $1 bash "${@:2}"
 fi
